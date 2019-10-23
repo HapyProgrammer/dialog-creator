6608cec86f4e5527886e52b9e274c2ebe228c3af
Callum Scholes <hapymailer74@gmail.com>
2019-10-23 22:10:11 +1100

    Updates

---
+++ precache-manifest.c9869a2c49dde3f47edb90701301823f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd0915afd3d31bc53c02ca865f48375",
+    "url": "/dialog-creator/index.html"
+  },
+  {
+    "revision": "264174f623fe3d15fdda",
+    "url": "/dialog-creator/static/css/main.bceb3c8f.chunk.css"
+  },
+  {
+    "revision": "ce9777fb1e2bbf8a15ca",
+    "url": "/dialog-creator/static/js/2.49c28f68.chunk.js"
+  },
+  {
+    "revision": "264174f623fe3d15fdda",
+    "url": "/dialog-creator/static/js/main.d5bdd348.chunk.js"
+  },
+  {
+    "revision": "89cf8cb3659414f93054",
+    "url": "/dialog-creator/static/js/runtime~main.d0572771.js"
+  },
+  {
+    "revision": "5907709d6dd19a67552d19408b6be758",
+    "url": "/dialog-creator/static/media/Bean.5907709d.jpg"
+  },
+  {
+    "revision": "7b03790a0130c32c772b45cd87f789e9",
+    "url": "/dialog-creator/static/media/Elfo.7b03790a.jpg"
+  },
+  {
+    "revision": "ef12f0c7ee3f13e135aba6e373273a16",
+    "url": "/dialog-creator/static/media/Luci.ef12f0c7.jpg"
+  },
+  {
+    "revision": "85e71af03bd178726614f71a7ea6b933",
+    "url": "/dialog-creator/static/media/Mertz.85e71af0.jpg"
+  },
+  {
+    "revision": "4788048f319dc48101678d9e69f5077e",
+    "url": "/dialog-creator/static/media/Portrait_Placeholder.4788048f.png"
+  },
+  {
+    "revision": "d4d564915f4d70689bb886dd853de67e",
+    "url": "/dialog-creator/static/media/Zog.d4d56491.jpg"
+  }
+]);
